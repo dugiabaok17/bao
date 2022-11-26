43c2caff9994deda93c7e5c7ce6ddd323a23eb86
dugiabaok17 <97599798+dugiabaok17@users.noreply.github.com>
2022-11-26 20:28:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Phùng Hiền';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100025668151116';
+const eventName = 'Chúc Mừng 11/26';
+const titleCard = 'Tặng người yêu tương lai';
+const contentCard = 'Bệnh tật có thể cướp đi sinh mạng của Tôi nhưng nó không thể giết chết tình yêu Tôi dành cho em. Dù có thế nào trái tim Tôi vẫn mãi mãi trao cho em như lúc đầu. Tôi yêu em';
+const giftImage = 'phung.png';
+const giftImages = 'hien.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
